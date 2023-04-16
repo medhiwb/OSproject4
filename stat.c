b98f3ca7dd25bbad738c3175f54b9c22d035cb36
medhiwb <115658023+medhiwb@users.noreply.github.com>
2023-04-15 22:11:01 -0400

    stat.c finished

---
+++ stat.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    struct stat filestat;
+    if (stat(argv[1], &filestat) == -1) {
+        perror("stat");
+        exit(EXIT_FAILURE);
+    }
+
+    // file size
+    printf("Size: %ld bytes\n", filestat.st_size);
+
+    // blocks allocated
+    printf("Blocks: %ld\n", filestat.st_blocks);
+
+    // link count
+    printf("Links: %ld\n", filestat.st_nlink);
+
+    // file permissions
+    printf("Permissions: ");
+    printf((S_ISDIR(filestat.st_mode)) ? "d" : "-");
+    printf((filestat.st_mode & S_IRUSR) ? "r" : "-");
+    printf((filestat.st_mode & S_IWUSR) ? "w" : "-");
+    printf((filestat.st_mode & S_IXUSR) ? "x" : "-");
+    printf((filestat.st_mode & S_IRGRP) ? "r" : "-");
+    printf((filestat.st_mode & S_IWGRP) ? "w" : "-");
+    printf((filestat.st_mode & S_IXGRP) ? "x" : "-");
+    printf((filestat.st_mode & S_IROTH) ? "r" : "-");
+    printf((filestat.st_mode & S_IWOTH) ? "w" : "-");
+    printf((filestat.st_mode & S_IXOTH) ? "x" : "-");
+    printf("\n");
+
+    // file inode
+    printf("Inode: %ld\n", filestat.st_ino);
+
+    return 0;
+}
